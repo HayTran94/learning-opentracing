16abd61bc095fea0b829bd323ed23aa81e126cc3
Hay Tran Van <haytv@vng.com.vn>
2021-05-17 14:25:02 +0700

    add lession 4

---
+++ lession4/formatter/formatter.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/uber/jaeger-client-go"
+	"github.com/uber/jaeger-client-go/config"
+	"io"
+	"net/http"
+	glog "log"
+	"time"
+)
+
+
+func initJaeger(service string) (opentracing.Tracer, io.Closer) {
+	cfg := &config.Configuration{
+		ServiceName: service,
+		Sampler: &config.SamplerConfig{
+			Type:  "const",
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans: true,
+		},
+	}
+	tracer, closer, err := cfg.NewTracer(config.Logger(jaeger.StdLogger))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot int Jaeger: %v\n", err))
+	}
+	return tracer, closer
+}
+
+func main() {
+	tracer, closer := initJaeger("formatter")
+	defer closer.Close()
+
+	http.HandleFunc("/format", func(w http.ResponseWriter, r *http.Request) {
+		spanCtx, _ := tracer.Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(r.Header))
+		span := tracer.StartSpan("format", ext.RPCServerOption(spanCtx))
+		defer span.Finish()
+
+		helloTo := r.FormValue("helloTo")
+		greeting := span.BaggageItem("greeting")
+		if greeting == "" {
+			greeting = "Hello"
+		}
+
+		helloStr := fmt.Sprintf("%s, %s!", greeting, helloTo)
+
+		time.Sleep(time.Millisecond * 634)
+		span.LogFields(
+			log.String("event", "string-format"),
+			log.String("value", helloStr),
+		)
+
+		w.Write([]byte(helloStr))
+	})
+
+	glog.Fatal(http.ListenAndServe(":8081", nil))
+}
